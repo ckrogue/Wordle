d51d368b6e575a243fe306dc7966179f54f4698b
colinkrogue <u1243862@umail.utah.edu>
2023-06-12 09:13:05 -0600

    started working on text box

---
+++ Wordle/Wordle/GameState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Wordle
+{
+    public class GameState
+    {
+        public int row;
+        
+        public GameState()
+        {
+            row = 1;
+        }
+
+        public char[] textBoxChanged(string newText)
+        {
+            //https://kodify.net/csharp/strings/remove-whitespace/
+            newText = String.Concat(newText.Where(c => !Char.IsWhiteSpace(c)));
+            char[] word = newText.ToCharArray();
+            return word;
+            
+        }
+
+        
+    }
+}
+
--- Wordle/Wordle/MainPage.xaml.cs
+++ Wordle/Wordle/MainPage.xaml.cs
@@ -2,24 +2,48 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+	List<HorizontalStackLayout> words;
+	GameState game;
 	public MainPage()
 	{
 		InitializeComponent();
+		initializeGame();
+		game = new GameState();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void initializeGame()
 	{
-		count++;
+        words = new List<HorizontalStackLayout>();
+        for (int i = 0; i < 6; i++)
+		{
+			HorizontalStackLayout word = new HorizontalStackLayout();
+			word.Spacing = 10;
+			word.HorizontalOptions = LayoutOptions.Center;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+            for (int j = 0; j < 5; j++)
+			{
+				Label temp = new Label();
+				temp.HeightRequest = 100;
+				temp.WidthRequest = 100;
+				temp.BackgroundColor = Colors.MediumPurple;
+				word.Add(temp);
+			}
+			
+			Board.Add(word);
+			words.Add(word);
+		}
 	}
+
+    void typeBox_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+    }
+
+    void typeBox_Completed(System.Object sender, System.EventArgs e)
+    {
+    }
+	public void slay() { }
+
 }
 
 
