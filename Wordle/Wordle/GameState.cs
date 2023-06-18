ea88d8f394c0761ab302c92c6774024e276db17b
colinkrogue <u1243862@umail.utah.edu>
2023-06-18 17:14:16 -0600

    Added changing between rows after hitting enter, and color coding

--- Wordle/Wordle/GameState.cs
+++ Wordle/Wordle/GameState.cs
@@ -4,19 +4,46 @@ namespace Wordle
     public class GameState
     {
         public int row;
+        private string word;
         
         public GameState()
         {
-            row = 1;
+            row = 0;
+            word = "soare";
         }
 
-        public char[] textBoxChanged(string newText)
+        public char[] getText(string newText)
         {
             //https://kodify.net/csharp/strings/remove-whitespace/
             newText = String.Concat(newText.Where(c => !Char.IsWhiteSpace(c)));
             char[] word = newText.ToCharArray();
             return word;
             
+        }
+
+        public int[] getCorrect(string text)
+        {
+            char[] userWord = text.ToCharArray();
+            char[] correctWord = word.ToCharArray();
+            int[] correct = new int[word.Length];
+            for(int i = 0; i < word.Length; i++)
+            {
+                if (userWord[i] == correctWord[i])
+                {
+                    correct[i] = 2;
+                }
+
+                else if (word.Contains(userWord[i]))
+                {
+                    correct[i] = 1;
+                }
+
+                else
+                {
+                    correct[i] = 0;
+                }
+            }
+            return correct;
         }
 
         
--- Wordle/Wordle/MainPage.xaml.cs
+++ Wordle/Wordle/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 
 public partial class MainPage : ContentPage
 {
-	List<HorizontalStackLayout> words;
+	List<List<Label>> words;
 	GameState game;
 	public MainPage()
 	{
@@ -13,9 +13,10 @@ public partial class MainPage : ContentPage
 
 	private void initializeGame()
 	{
-        words = new List<HorizontalStackLayout>();
+        words = new List<List<Label>>();
         for (int i = 0; i < 6; i++)
 		{
+			List<Label> labels = new List<Label>();
 			HorizontalStackLayout word = new HorizontalStackLayout();
 			word.Spacing = 10;
 			word.HorizontalOptions = LayoutOptions.Center;
@@ -27,22 +28,73 @@ public partial class MainPage : ContentPage
 				temp.HeightRequest = 100;
 				temp.WidthRequest = 100;
 				temp.BackgroundColor = Colors.MediumPurple;
+				temp.TextColor = Colors.Black;
+				temp.HorizontalTextAlignment = TextAlignment.Center;
+				temp.VerticalTextAlignment = TextAlignment.Center;
+				temp.FontSize = 30;
 				word.Add(temp);
+				labels.Add(temp);
 			}
 			
 			Board.Add(word);
-			words.Add(word);
+			words.Add(labels);
 		}
 	}
 
     void typeBox_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
     {
+		
+		updateBoard(game.getText(typeBox.Text), game.row);
     }
+	private void updateBoard(char[] text, int row)
+	{
+		int i = 0;
+		int length = text.Count();
+		foreach(Label letter in words[row])
+		{
+			if(i < length)
+			{
+                letter.Text = text[i].ToString();
+            }
+			else
+			{
+				letter.Text = "";
+			}
+
+			i++;
+		}
+
+	}
+
 
     void typeBox_Completed(System.Object sender, System.EventArgs e)
     {
+		updateRowColors(game.row, game.getCorrect(typeBox.Text));
+		game.row = game.row + 1;
+		typeBox.Text = "";
+		typeBox.Focus();
     }
-	public void slay() { }
+	public void updateRowColors(int row, int[] correct)
+	{
+		for(int i = 0; i < correct.Count(); i++)
+		{
+			if (correct[i] == 0)
+			{
+				words[row][i].BackgroundColor = Colors.Red;
+			}
+            else if (correct[i] == 1)
+            {
+                words[row][i].BackgroundColor = Colors.Yellow;
+            }
+            else
+            {
+                words[row][i].BackgroundColor = Colors.Green;
+            }
+
+
+        }
+
+    }
 
 }
 
